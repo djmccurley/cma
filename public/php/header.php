4f28d91a5efac90989330b47df1a34e58fa022c0
Dan McCurley <djmccurley@gmail.com>
2017-07-22 21:38:41 -0500

    added first artist page

---
+++ public/php/header.php
@@ -0,0 +1,48 @@
+<?php 
+
+echo    '<header class="clearfix">
+      <div class="asphalt"></div>
+      <div class="border"></div>
+      <div class="header_wrapper">
+        <div class="flex_container">
+          <h1 class="visuallyhidden">Celtic Music Association</h1>
+          <img src="img/cma_logo_1.svg" alt="Celtic Music Association Logo" class="logo">
+          <div class="menu_toggle">Menu</div>
+        </div>
+        <nav class="nav_wrapper clearfix">
+
+            <ol>
+              <li><a href="concerts.php">Concerts</a>
+                <ol class="submenu clearfix">
+                  <li><a href="concerts.php">2017 – 2018 Season</a></li>
+                  <li><a href="skippers.php">Skipper\'s Alley</a></li>
+                  <li><a href="tallymoore.php">Tallymoore</a></li>
+                  <li><a href="daimh.php">Dàimh</a></li>
+                  <li><a href="socks.php">Socks in the Frying Pan</a></li>
+                </ol>   
+              </li>
+              <li><a href="tickets.php">Buy Tickets</a></li>
+              <li><a href="support.php">Support the CMA</a>
+                <ol class="submenu clearfix">
+                  <li><a href="support.php">Join the CMA</a></li>
+                  <li><a href="support.php">Become a Sponsor</a></li>
+                  <li><a href="support.php">Donate to CMA</a></li>
+                  <li><a href="support.php">Advertise with CMA</a></li>
+                </ol> 
+              </li>
+              <li><a href="#!">About</a>
+                <ol class="submenu clearfix">
+                  <li><a href="#!">Newsletter</a></li>
+                  <li><a href="#!">Board &amp; Members</a></li>
+                  <li><a href="#!">History</a></li>
+                  <li><a href="#!">More Celtic Music</a></li>
+                </ol> 
+              </li>  
+              <li><a href="#!">Contact</a></li>
+            </ol>
+        </nav> 
+      </div>   
+    </header>
+
+' ;
+  ?>
