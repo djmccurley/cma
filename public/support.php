1eabc24aa758f2426e6094c27bb0d375e86e3bec
Dan McCurley <djmccurley@gmail.com>
2017-07-23 12:28:00 -0500

    added tickets and support page

---
+++ public/support.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Celtic Music Association</title>
+      <meta name="description" content="Celtic Music Association, Des Moines, Iowa. Central Iowa's premiere Celtic music organization.">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <link rel="apple-touch-icon" href="apple-touch-icon.png">
+      <!-- Place favicon.ico in the root directory -->
+
+      <link href="https://fonts.googleapis.com/css?family=Raleway:300,600" rel="stylesheet">
+      <link rel="stylesheet" href="css/css.css">
+      <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+  </head>
+  <body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <?php include 'php/header.php' ; ?>
+
+    <main>
+        <article class="leader">
+            <h1>Support the CMA</h1>
+        </article>
+
+        <article class="content_pane">
+            <div class="content_wrapper">
+                <h2>Become a Friend</h2>
+                  <p>Complete the form below to become a Friend of the Celtic Music Association:</p>
+                  <a href="http://www.thecma.org" class="info_link">Membership form (PDF)</a>
+
+                  
+            </div>
+        </article>
+
+        <aside class="ad">
+            <h2>Our Sponsors</h2>
+            <img src="http://placehold.it/800x300">
+        </aside>    
+    </main>
+
+    <footer class="clearfix">
+        <div class="border"></div>
+        <!-- <div class="asphalt"> -->
+            <p>Copyright 2017</p>
+            <a href="#!">Like Us on Facebook</a>
+        </div>
+    </footer>
+
+    <script src="js/vendor/jquery-3.2.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+  </body>
+  </html>
---
+++ public/tickets.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Celtic Music Association</title>
+      <meta name="description" content="Celtic Music Association, Des Moines, Iowa. Central Iowa's premiere Celtic music organization.">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <link rel="apple-touch-icon" href="apple-touch-icon.png">
+      <!-- Place favicon.ico in the root directory -->
+
+      <link href="https://fonts.googleapis.com/css?family=Raleway:300,600" rel="stylesheet">
+      <link rel="stylesheet" href="css/css.css">
+      <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+  </head>
+  <body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <?php include 'php/header.php' ; ?>
+
+    <main>
+        <article class="leader">
+            <h1>Tickets</h1>
+        </article>
+
+        <article class="content_pane">
+            <div class="content_wrapper">
+                <h2>Ticket Info</h2>
+                <p>Season tickets $88 (until Oct. 20)</p> 
+                <p>$25 single ticket in advance</p>
+                 <p>$30 at the door</p>
+            </div>
+        </article>
+
+        <article class="content_pane">
+            <div class="content_wrapper tickets">
+                <h2>Ticket Outlets</h2>
+                  <h3>Celtic Music Association</h3>
+                  <p>515-771-2215 (check only)</p>
+                  <p>P. O. Box 30001, Des Moines, Iowa 50310</p>
+                  <a href="http://www.thecma.org">www.thecma.org</a>
+
+                  <h3>Holy Trinity Parish Office</h3>
+                  <p>515-255-3162 (cash or check only) </p>
+                  <p>2926 Beaver Ave., Des Moines, IA 50310</p>
+                  <a href="http://www.htschool.org">www.htschool.org</a>
+
+                  <h3>Cooney’s Tavern</h3>
+                  <p>515-255-5566 (cash or check only)</p>
+                  <p>3708 Beaver Ave., Des Moines, IA 50310</p>
+                  <a href="http://www.cooneystavern.com">www.cooneystavern.com</a>
+
+                  <h3>Up Tempo Music</h3>
+                  <p>515-277-1045 (cash or check only)</p>
+                  <p>2714 Beaver Ave., Des Moines, IA 50310</p>
+                  <a href="http://www.uptempo-music.com">www.uptempo-music.com</a>
+
+                  <h3>A Celtic Tradition</h3>
+                  <p>515-278-8302 (cash or check only)</p>
+                  <p>7672 Hickman Rd., Windsor Heights, IA 50324 </p>
+                  <a href="http://www.2celts.com">www.2celts.com</a>
+                  
+                  <h3>Red Truck Tickets (credit and debit)</h3>
+                  <a href="http://www.redtrucktickets.com">www.redtrucktickets.com</a>
+
+                  <h3>Mainstream Boutique</h3>
+                  <p>515-277-4117 (cash or check only)</p>
+                  <p>114 5th St., West Des Moines, IA</p>
+                  <a href="http://www.mainstreamboutique.com">www.mainstreamboutique.com</a>
+                  
+                  <h3>For Hoyt Sherman concerts:</h3>
+                  <h4>Hoyt Sherman Box Office</h4>
+                  <p>515-244-0507 (credit and debit)</p>
+                  <p>1501 Woodland Ave. Des Moines, IA</p>
+                  <a href="http://www.hoytsherman.org">www.hoytsherman.org</a>
+                  
+                  <h4>Ticketmaster (credit and debit)</h4>
+                  <a href="http://www.ticketmaster.com">www.ticketmaster.com</a>
+            </div>
+        </article>
+
+        <aside class="ad">
+            <h2>Our Sponsors</h2>
+            <img src="http://placehold.it/800x300">
+        </aside>    
+    </main>
+
+    <footer class="clearfix">
+        <div class="border"></div>
+        <!-- <div class="asphalt"> -->
+            <p>Copyright 2017</p>
+            <a href="#!">Like Us on Facebook</a>
+        </div>
+    </footer>
+
+    <script src="js/vendor/jquery-3.2.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+  </body>
+  </html>
